bd2e97d0f35ee3e33a864bbf409ce1a34002ca30
Joost Bloemsma <joost.a@bloemsma.net>
2022-05-16 14:18:12 +0200

    Added secrets.h again. Github compiler does not use __has_include

---
+++ lib/mqtt-now/src/secrets.h
@@ -0,0 +1,26 @@
+/**
+ * @file secrets-dummy.h
+ * @author Joost Bloemsma (joost.a@bloemsma.net)
+ * @brief 
+ * @version 0.1
+ * @date 2022-05-16
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ * Copy this file to secrets.h in this same directory, and fill in your own values.
+ * The new file will be included automatically
+ */
+# pragma once
+#ifndef __SECRETS_H__
+#define __SECRETS_H__
+
+#define MQTT_HOST "10.0.1.1"
+#define MQTT_PORT 1883
+#define MQTT_ID "mqtt_now"
+#define MQTT_USER "mqtt_user"
+#define MQTT_PW "mqtt_pw"
+
+#define WIFI_SSID "myssid_name"
+#define WIFI_PW "myssid_pw"
+
+#endif // __SECRETS_H__
