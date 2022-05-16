3a4a3503153cffc3580ac8e4392df5c893f6df59
Joost Bloemsma <joost.a@bloemsma.net>
2022-05-16 14:12:03 +0200

    Added instructions for secrets.h

--- lib/mqtt-now/src/secrets-dummy.h
+++ lib/mqtt-now/src/secrets-dummy.h
@@ -1,9 +1,19 @@
+/**
+ * @file secrets-dummy.h
+ * @author Joost Bloemsma (joost.a@bloemsma.net)
+ * @brief 
+ * @version 0.1
+ * @date 2022-05-16
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ * Copy this file to secrets.h in this same directory, and fill in your own values.
+ * The new file will be included automatically
+ */
 # pragma once
 #ifndef __SECRETS_H__
 #define __SECRETS_H__
 
-// Fill these defines with your own values
-// These are dummy values
 #define MQTT_HOST "10.0.1.1"
 #define MQTT_PORT 1883
 #define MQTT_ID "mqtt_now"
